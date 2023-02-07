3149ce6d993f30974709b94a9867968c6185e50c
Igor Zhivilo <warolv@gmail.com>
2023-02-07 19:25:49 +0200

    Add guide of how to create pod using python and k8s library

---
+++ k8s-create-pod-python/pod_handler.py
@@ -0,0 +1,63 @@
+from kubernetes import client, config
+import os
+
+k8s_endpoint = os.getenv('APISERVER')
+bearer_token = os.getenv('TOKEN')
+
+def init_client(k8s_endpoint, bearer_token):
+    k8s_conf = client.Configuration()
+    k8s_conf.host = k8s_endpoint
+    k8s_conf.verify_ssl = False
+    k8s_conf.api_key = {"authorization": "Bearer " + bearer_token}
+    client.Configuration.set_default(k8s_conf)
+    v1 = client.CoreV1Api()
+    
+    return v1
+
+# def init_client():
+#     # Configs can be set in Configuration class directly or using helper utility
+#     config.load_kube_config()
+#     v1 = client.CoreV1Api()
+    
+#     return v1
+
+def list_pods(v1):
+    print("Listing pods with their IPs:")
+    ret = v1.list_pod_for_all_namespaces(watch = False)
+    for i in ret.items:
+        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+# create busybox pod in default namespace
+def create_pod(v1):
+    pod_manifest = {
+        'apiVersion': 'v1',
+        'kind': 'Pod',
+        'metadata': {
+            'name': 'busybox'
+        },
+        'spec': {
+            'containers': [{
+                'image': 'busybox',
+                'name': 'sleep',
+                "args": [
+                    "/bin/sh",
+                    "-c",
+                    "while true;do date;sleep 5; done"
+                ]
+            }]
+        }
+    }
+    resp = v1.create_namespaced_pod(body = pod_manifest, namespace = 'default')
+    print(resp)
+
+def delete_pod(v1):
+    resp = v1.delete_namespaced_pod(
+        name = 'busybox',
+        namespace = "default")
+    print(resp)
+        
+
+v1 = init_client(k8s_endpoint, bearer_token)
+# list_pods(v1)
+# create_pod(v1)
+delete_pod(v1)
